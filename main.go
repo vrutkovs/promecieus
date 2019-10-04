b59f2d2984f68d3acb87976bfb5e2ebba130208c
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-10-04 11:28:55 +0200

    main.go: get metrics link

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"strings"
 )
 
 // Env holds references to useful objects in router funcs
@@ -11,6 +13,17 @@ type Env struct{}
 
 func (e *Env) create(c *gin.Context) {
 	url := c.PostForm("url")
+	log.Println(fmt.Sprintf("url: %s", url))
+
+	// Get artifacts link
+	artifactsUrl := strings.ReplaceAll(url, "https://prow.svc.ci.openshift.org/view/", "https://gcsweb-ci.svc.ci.openshift.org")
+	log.Println(fmt.Sprintf("artifacts: %s", artifactsUrl))
+
+	// Get a link to prometheus metadata
+	// TODO: aws is hardcoded :/
+	metricsTar := fmt.Sprintf("%s/artifacts/e2e-aws/metrics/", artifactsUrl)
+	log.Println(fmt.Sprintf("metricsTar: %s", metricsTar))
+
 	c.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("foo %s", url),
 	})

8bb83e1d41e804c4c8f587438cf9689d14a7f9ac
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-10-04 11:13:09 +0200

    Remove nonsense from main.go

--- main.go
+++ main.go
@@ -3,26 +3,19 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"os"
 )
 
 // Env holds references to useful objects in router funcs
-type Env struct {
-	route string
-}
+type Env struct{}
 
 func (e *Env) create(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"url": "https://foo",
+		"message": "foo",
 	})
 }
 
 func main() {
-	// Check neccessary env vars are set
-	route := os.Getenv("ROUTE")
-	if len(route) == 0 {
-		panic("ROUTE env var is not set")
-	}
+	// Get apps domain from the route
 
 	// setup webhook listener
 	r := gin.Default()
